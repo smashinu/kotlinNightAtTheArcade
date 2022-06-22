cfc317bb1155d439cbb22064fe142bf107bb67c3
smashinu <30433218+smashinu@users.noreply.github.com>
2022-06-22 16:03:40 +0800

    progress
    progress

--- main.kt
+++ main.kt
@@ -1,8 +1,16 @@
-fun main(){
-  
-    var Card1 = Card(50)
-    var Card2 = Card(70)
 
+
+
+
+fun main(){
+    var Card1 = Card()
+    var Card2 = Card()
+    var Game1 = Game(5)
+    var Game2 = Game(7)
+    var Terminal1 = Terminal()
+    var Terminal2 = Terminal()
+    Terminal1.addCredits(Card1, 50)
+    Game1.play(Card1)
 }
 
 var CardIdentity = 0
@@ -13,24 +21,57 @@ fun IncremenetCardIdentity(): Int{
 }
 
 
-class Game{
+class Game(val InCost: Int){
+    var Cost: Int
 
+    init{
+        this.Cost = InCost
+    }
+
+    fun play(Card: Card){
+        if(Card.credits > Cost){
+            removeCredits(Card, Cost)
+            Card.tickets += (0..50).random()
+            println("Tickets Awarded: " + Card.tickets)
+        }
+        else{
+            println("Unable to play this game due to inneficient funds")
+        }
+    }
+
+    fun removeCredits(Card: Card,removalcredits: Int){
+        if((Card.credits - removalcredits) < 0){
+            Card.credits = 0
+        }
+        else{
+            Card.credits = Card.credits - removalcredits
+        }
+    }
 }
 
 class Prize{
-
+  
 }
 
-class Termainl{
+class Terminal(){
+    init{
+
+    }
+ 
+
+    fun addCredits(Card: Card,addcredits: Int){  
+        Card.credits = Card.credits + (addcredits * 2)
+        println("Current balance is:" + Card.credits)
+    }
 }
 
-class Card(val initialBalance: Int){
-    var balance: Int
+class Card(){
+    var credits: Int
     var Id: Int
+    var tickets: Int
     init{
         this.Id = IncremenetCardIdentity()
-        this.balance = initialBalance
-        println(this.Id)
+        this.credits = 0
+        this.tickets = 0
     }
-
 }
